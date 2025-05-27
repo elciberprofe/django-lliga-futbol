410ae630c2246a85cacf0751c67ccdc0aeafe1a8
Xavi <elciberprofessor@gmail.com>
2025-05-28 01:35:57 +0200

    Primer commit: Lliga de Futbol (autogenerada)

---
+++ lliga/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import *
+# Create your views here.
+
+def classificacio_menu(request):
+    queryset = Lliga.objects.all()
+    return render(request,"classificacio_menu.html",
+                  {"lligues":queryset})
+
+def classificacio(request, lliga_id):
+    lliga = Lliga.objects.get(pk=lliga_id)
+    equips = lliga.equips.all()
+    classi = []
+
+    # calculem punts en llista de tuples (equip,punts)
+    for equip in equips:
+        punts = 0
+        for partit in lliga.partits.filter(local=equip):
+            if partit.gols_local() > partit.gols_visitant():
+                punts += 3
+            elif partit.gols_local() == partit.gols_visitant():
+                punts += 1
+        for partit in lliga.partits.filter(visitant=equip):
+            if partit.gols_local() < partit.gols_visitant():
+                punts += 3
+            elif partit.gols_local() == partit.gols_visitant():
+                punts += 1
+        classi.append( (punts,equip.nom) )
+    # ordenem llista (els punts davant de la tupla es qui marca el criteri d'ordre)
+    classi.sort(reverse=True)
+    return render(request,"classificacio.html",
+                {
+                    "classificacio":classi,
+                })
